4fdca43ba85baafb1b2a84dcc44e61c9e63a222c
Igor Ivlev <igor.ivlev@verkstedt.com>
2019-05-09 10:00:54 +0200

    Updates

---
+++ component---src-templates-slide-js-1ebfcae8df3f5784c4ab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{162:function(e,t,n){"use strict";n.r(t),n.d(t,"query",function(){return s});var a=n(0),r=n.n(a);t.default=function(e){var t=e.data,n=e.transition;return r.a.createElement("div",{className:"slideItem"},r.a.createElement("div",{style:n&&n.style,dangerouslySetInnerHTML:{__html:t.slide.html}}))};var s="2716726613"}}]);
+//# sourceMappingURL=component---src-templates-slide-js-1ebfcae8df3f5784c4ab.js.map
